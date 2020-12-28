b98f7695687e76102c09ac213c50aa1238d8a2bc
Galiwango Ananiya <galiwango101@gmail.com>
2020-12-29 01:35:15 +0300

    finish QUIZ 4

--- databases.sql
+++ databases.sql
@@ -1,44 +1,35 @@
 --QUIZ 1
-SELECT name, population
-  FROM world
+SELECT name, population FROM world
   WHERE population BETWEEN 1000000 AND 1250000;
 
-SELECT name, population
-  FROM world
+SELECT name, population FROM world
   WHERE name LIKE "Al%";
 
 SELECT name FROM world
   WHERE name LIKE '%a' OR name LIKE '%l';
 
-SELECT name,length(name)
-  FROM world
+SELECT name,length(name) FROM world
   WHERE length(name)=5 and region='Europe';
 
 SELECT name, area*2 FROM world WHERE population = 64000;
 
-SELECT name, area, population
-  FROM world
+SELECT name, area, population FROM world
   WHERE area > 50000 AND population < 10000000;
 
-SELECT name, population/area
-  FROM world
+SELECT name, population/area FROM world
   WHERE name IN ('China', 'Nigeria', 'France', 'Australia');
 
 --QUIZ 2
-SELECT name
-  FROM world
+SELECT name FROM world
   WHERE name LIKE 'U%';
 
-SELECT population
-  FROM world
+SELECT population FROM world
   WHERE name = 'United Kingdom';
 
-SELECT continent 
-  FROM world 
+SELECT continent FROM world 
   WHERE 'name' = 'France';
 
-SELECT name, population
-  FROM world
+SELECT name, population FROM world
   WHERE continent IN ('Europe', 'Asia');
 
 SELECT name FROM world
@@ -69,3 +60,28 @@ SELECT DISTINCT yr FROM nobel
 
 SELECT subject, COUNT(subject) FROM nobel 
   WHERE yr ='1960' GROUP BY subject;
+
+--QUIZ 4
+SELECT region, name, population FROM bbc x
+  WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0);
+
+SELECT name,region,population FROM bbc x
+  WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0);
+
+SELECT name, region FROM bbc x
+  WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region = x.region AND y.name != x.name);
+
+SELECT name FROM bbc
+  WHERE population > (SELECT population FROM bbc WHERE name='United Kingdom')
+    AND region IN (SELECT region FROM bbc WHERE name = 'United Kingdom');
+
+SELECT name FROM bbc
+ WHERE gdp > (SELECT MAX(gdp) FROM bbc WHERE region = 'Africa');
+
+SELECT name FROM bbc
+  WHERE population < (SELECT population FROM bbc WHERE name='Russia')
+    AND population > (SELECT population FROM bbc WHERE name='Denmark');
+
+SELECT name FROM bbc
+  WHERE population > ALL (SELECT MAX(population) FROM bbc WHERE region = 'Europe')
+    AND region = 'South Asia';
