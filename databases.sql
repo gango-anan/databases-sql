c3b43058d491aa4db2c447dd08911818a9a81cbd
Galiwango Ananiya <galiwango101@gmail.com>
2020-12-29 02:59:04 +0300

    Complete Quiz 5

--- databases.sql
+++ databases.sql
@@ -85,3 +85,22 @@ SELECT name FROM bbc
 SELECT name FROM bbc
   WHERE population > ALL (SELECT MAX(population) FROM bbc WHERE region = 'Europe')
     AND region = 'South Asia';
+
+--QUIZ 5
+SELECT SUM(population) FROM bbc WHERE region = 'Europe';
+
+SELECT COUNT(name) FROM bbc WHERE population < 150000;
+
+-- AVG(), COUNT(), MAX(), MIN(), SUM()
+
+-- No result due to invalid use of the WHERE function
+
+SELECT AVG(population) FROM bbc WHERE name IN ('Poland', 'Germany', 'Denmark');
+
+ SELECT region, SUM(population)/SUM(area) AS density FROM bbc GROUP BY region;
+
+ SELECT name, population/area AS density FROM bbc WHERE population = (SELECT MAX(population) FROM bbc);
+
+SELECT region, SUM(area) FROM bbc 
+  GROUP BY region
+  HAVING SUM(area)<= 20000000
