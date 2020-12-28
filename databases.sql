f2e15f97e4645445c96c3e5e8844ffcdb3e6973d
Galiwango Ananiya <galiwango101@gmail.com>
2020-12-29 00:16:19 +0300

    finish QUIZ 3

--- databases.sql
+++ databases.sql
@@ -47,3 +47,25 @@ SELECT name FROM world
 SELECT name FROM world
   WHERE continent = 'South America'
     AND population > 40000000;
+
+--QUIZ 3
+SELECT winner FROM nobel
+  WHERE winner LIKE 'C%' AND winner LIKE '%n';
+
+SELECT COUNT(subject) FROM nobel
+  WHERE subject = 'Chemistry' AND yr BETWEEN 1950 and 1960;
+
+SELECT COUNT(DISTINCT yr) FROM nobel
+  WHERE yr NOT IN (SELECT DISTINCT yr FROM nobel WHERE subject = 'Medicine');
+
+SELECT subject, winner FROM nobel WHERE winner LIKE 'Sir%' AND yr LIKE '196%';
+
+SELECT yr FROM nobel
+  WHERE yr NOT IN(SELECT yr FROM nobel WHERE subject IN ('Chemistry','Physics'));
+
+SELECT DISTINCT yr FROM nobel
+  WHERE subject='Medicine' AND yr NOT IN(SELECT yr FROM nobel WHERE subject='Literature')
+    AND yr NOT IN (SELECT yr FROM nobel WHERE subject='Peace');
+
+SELECT subject, COUNT(subject) FROM nobel 
+  WHERE yr ='1960' GROUP BY subject;
